ce6ad1f5260e43041314a61942ff94bb4c504c7c
Lior Tal <lior.tal13@gmail.com>
2021-11-13 18:18:48 +0200

    2.1 humburger

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -1,18 +1,24 @@
+import appNav from "./app-nav.js"
 export default {
     template: `
         <header class="app-header flex space-between align-center">
             <div class="logo flex align-center">
-                AppSuss
+            <router-link to="/" active-class="active-link" exact>AppSuss</router-link> 
+                
                 <img class="main-logo" src="img/logoGif.gif" alt="">
             </div>
             
-            <nav class="main-nav flex align-center">
-                <router-link to="/" active-class="active-link" exact>Home</router-link> 
+            <!-- <nav class="main-nav flex align-center">
+                
                 <router-link to="/book">Books</router-link> 
                 <router-link to="/email">Email</router-link>
                 <router-link to="/notes">Notes</router-link>
                 <router-link to="/about">About</router-link>
-            </nav>
+            </nav> -->
+            <app-nav />
         </header>
     `,
+    components: {
+        appNav,
+    },
 }
---
+++ js/cmps/app-nav.js
@@ -0,0 +1,86 @@
+import appNavItem from './app-nav-item.js'
+
+export default {
+    props: [],
+    template: `
+        <nav
+        class="app-nav"
+        >
+
+            <i class="menu-icon fas fa-th fa-lg"
+            @click.stop="onToggleMenu" 
+            ></i>
+            
+            <transition
+            enter-active-class="animate__animated animate__fadeInRight"
+            leave-active-class="animate__animated animate__fadeOutRight"
+            >
+            <!-- enter-active-class="animate__animated animate__bounceIn"
+            leave-active-class="animate__animated animate__bounceOut" -->
+            <!-- enter-active-class="animate__animated animate__fadeInTopRight"
+            leave-active-class="animate__animated animate__fadeOutTopRight" -->
+                <section 
+                class="menu-container"
+                @click="onToggleMenu"
+                v-if="isNavOpen">
+                
+                <app-nav-item 
+                v-for="item in items"
+                :item="item"
+                />
+            </section>
+            </transition>
+            
+        </nav>
+
+    `,
+    data() {
+        return {
+            isNavOpen: false,
+            items: [{
+                    icon: 'fas fa-envelope fa-2x',
+                    title: 'Email',
+                    routerUrl: '/email',
+                    color: 'black'
+                },
+                {
+                    icon: 'fas fa-sticky-note fa-2x',
+                    title: 'Keep',
+                    routerUrl: '/notes',
+                    color: 'black'
+                },
+                {
+                    icon: 'fas fa-info fa-2x',
+                    title: 'About',
+                    routerUrl: '/about',
+                    color: 'black'
+                },
+                {
+                    icon: 'fas fa-book fa-2x',
+                    title: 'Books',
+                    routerUrl: '/book',
+                    color: 'black'
+                },
+                {
+                    icon: 'fas fa-home fa-2x',
+                    title: 'Home Page',
+                    routerUrl: '/',
+                    color: 'black'
+                },
+
+            ],
+
+        }
+    },
+    methods: {
+        onToggleMenu() {
+            this.isNavOpen = !this.isNavOpen
+        }
+    },
+    components: {
+        appNavItem,
+    },
+    created() {
+        document.addEventListener('click', () => { this.isNavOpen = false })
+    },
+}
